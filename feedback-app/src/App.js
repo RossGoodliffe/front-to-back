0ebf0814343bc7f7c63ef686222cc1950a88628f
Ross <rossgoodliffe@gmail.com>
2022-02-24 21:46:29 +0000

    section 5: 27 completed

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -5,6 +5,7 @@ import Header from "./components/Header";
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
+import AboutIconLink from './components/AboutIconLink'
 import AboutPage from './pages/AboutPage';
 
 // Importing data
@@ -44,6 +45,8 @@ function App() {
                 </Route>
                 <Route path="/about" element={<AboutPage />} />
             </Routes>
+
+            <AboutIconLink />
         </div>
         </Router>
     )
