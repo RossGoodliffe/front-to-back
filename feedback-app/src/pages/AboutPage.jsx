00c28dc9b5ed28541ef34d3777f6adbad33ac489
Ross <rossgoodliffe@gmail.com>
2022-03-22 18:48:25 +0000

    Changes to about page

--- feedback-app/src/pages/AboutPage.jsx
+++ feedback-app/src/pages/AboutPage.jsx
@@ -7,7 +7,7 @@ function AboutPage() {
         <Card>
             <div className="about">
                 <h1>About This Project</h1>
-                <p>This is a React app to leave feedback for a product or service</p>
+                <p>This is a React application to leave feedback for a product or service</p>
                 <p>Version: 1.0.0</p>
                 <p><Link to="/">Back to Home</Link></p>
             </div>
